03ab3532b53e3708131ad8ec90e9731ba8d87af0
Caroline Cahilly <carolinecahilly@DN0a224967.SUNet>
2024-11-06 13:34:13 -0800

    splitting data

---
+++ clap-to-t5/t5-audio-to-text.py
@@ -0,0 +1,4 @@
+# General Approach
+# Create a projection layer to transform the (512,) embeddings to the T5 input dimension
+# and then set up a model architecture that feeds these transformed embeddings into T5. 
+
